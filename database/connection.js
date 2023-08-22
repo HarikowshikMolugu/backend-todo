187e735ab9e6a0df7b216c63f0ae4a61a8611c81
harikowshik.molugu <harikowshik.molugu@gmail.com>
2023-08-22 20:33:29 +0530

    app

---
+++ database/connection.js
@@ -0,0 +1,20 @@
+const Sequelize = require("sequelize");
+const config = require("../config/config.json");
+const sequelize = new Sequelize(
+	config.database,
+	config.username,
+	config.password,
+	config
+);
+
+// Test the connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connected to PgAdmin');
+  })
+  .catch((error) => {
+    console.error('Failed to connect to PgAdmin:', error);
+  });
+
+module.exports = sequelize;
